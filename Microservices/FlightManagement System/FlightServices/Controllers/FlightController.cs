6e00c159490c950e0dd62e7e707320b7259d8f61
12ven <vengalraopydimari@gmail.com>
2026-04-15 18:08:50 +0530

    done

---
+++ Microservices/FlightManagement System/FlightServices/Controllers/FlightController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        private static List<object> flights = new List<object>
+        {
+            new { Id = 1, FlightNumber = "AI101", Source = "Hyderabad", Destination = "Delhi" },
+            new { Id = 2, FlightNumber = "AI202", Source = "Chennai", Destination = "Mumbai" }
+        };
+
+        [HttpGet("GetFlights")]
+
+        public IActionResult GetAll()
+        {
+            return Ok(flights);
+        }
+
+        [HttpGet("Get/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            var flight = flights.FirstOrDefault(f => (int)f.GetType().GetProperty("Id").GetValue(f) == id);
+            return Ok(flights);
+        }
+
+        [HttpPost("AddFlight")]
+
+        public IActionResult Insert([FromBody] object flight)
+        {
+            flights.Add(flight);
+            return Ok(flights);
+        }
+
+
+    }
+}
---
+++ Microservices/FlightManagement System/PassengerServices/Controllers/PassengerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PassengerServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassengerController : ControllerBase
+    {
+        private static List<object> passengers = new List<object>
+        {
+            new { PassengerId = 201, Name = "John", Age = 30 }
+        };
+
+        [HttpGet("GetPassengers")]
+
+        public IActionResult GetAll()
+        {
+            return Ok(passengers);
+        }
+
+        [HttpPost("AddPassenger")]
+
+        public IActionResult Insert([FromBody] object passenger)
+        {
+            passengers.Add(passenger);
+            return Ok(passengers);
+        }
+    }
+}
